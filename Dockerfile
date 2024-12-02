54b39d14818bdba062fd700eb5515104bde2ecfa
Timothy Small <smalls.online@icloud.com>
2024-12-02 12:03:17 -0500

    Improve multi-arch container building
    This should help with reducing the build time in GitHub Actions so that both `linux/amd64` and `linux/arm64` image variants are published in a reasonable amount of time.

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,33 @@
-FROM --platform=${TARGETPLATFORM} docker.io/library/rust:1-bullseye AS build
+FROM --platform=${BUILDPLATFORM} docker.io/library/rust:1-bullseye AS build
+
+ARG TARGETPLATFORM
+ARG TARGETARCH
 
 WORKDIR /app
 
 COPY . .
 
 RUN apt-get update \
-    && apt-get install -y libsqlite3-dev libsqlite3-0 \
+    && apt-get install -y \
+        g++-x86-64-linux-gnu \
+        libc6-dev-amd64-cross \
+        g++-aarch64-linux-gnu \
+        libc6-dev-arm64-cross \
+        pkg-config \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
     
-RUN cargo build --package "fediproto-sync" --release
+RUN chmod +x ./docker-build/build.sh \
+    && ./docker-build/build.sh
 
-FROM --platform=${TARGETPLATFORM} docker.io/library/debian:bullseye-slim
+FROM --platform=${TARGETARCH:-$BUILDPLATFORM} docker.io/library/debian:bullseye-slim
 
 RUN apt-get update \
     && apt-get install -y libsqlite3-0 ca-certificates \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
 
-COPY --from=build /app/target/release/fediproto-sync /usr/local/bin/fediproto-sync
+COPY --from=build /tmp/fediproto-sync /usr/local/bin/fediproto-sync
 
 WORKDIR /app
 
---
+++ docker-build/build.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+RUST_TARGET=""
+case "${TARGETPLATFORM}" in
+    "linux/amd64")
+        RUST_TARGET="x86_64-unknown-linux-gnu"
+        export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="x86_64-linux-gnu-gcc"
+        export CC_x86_64_unknown_linux_gnu="x86_64-linux-gnu-gcc"
+        export CXX_x86_64_unknown_linux_gnu="x86_64-linux-gnu-g++"
+
+        dpkg --add-architecture amd64
+        apt-get update
+        apt-get install -y libsqlite3-dev:amd64 libsqlite3-0:amd64
+        apt-get clean
+        rm -rf /var/lib/apt/lists/*
+        ;;
+    "linux/arm64")
+        RUST_TARGET="aarch64-unknown-linux-gnu"
+        export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER="aarch64-linux-gnu-gcc"
+        export CC_aarch64_unknown_linux_gnu="aarch64-linux-gnu-gcc"
+        export CXX_aarch64_unknown_linux_gnu="aarch64-linux-gnu-g++"
+
+        dpkg --add-architecture arm64
+        apt-get update
+        apt-get install -y libsqlite3-dev:arm64 libsqlite3-0:arm64
+        apt-get clean
+        rm -rf /var/lib/apt/lists/*
+        ;;
+    *)
+        echo "Unsupported platform: ${TARGETPLATFORM}"
+        exit 1
+        ;;
+esac
+
+echo "${RUST_TARGET}"
+
+rustup default nightly
+rustup target add --toolchain "nightly" "${RUST_TARGET}"
+cargo fetch --target "${RUST_TARGET}"
+cargo build --package "fediproto-sync" --release --target "${RUST_TARGET}"
+cp "./target/${RUST_TARGET}/release/fediproto-sync" "/tmp/fediproto-sync"
