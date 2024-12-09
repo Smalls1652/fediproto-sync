8746104c65c4a87ee97d7ab0c9ea9f3508c3ff85
Timothy Small <smalls.online@icloud.com>
2024-12-09 14:05:56 -0500

    Allow pinned posts when fetching Mastodon statuses (#9)

--- fediproto-sync/src/mastodon.rs
+++ fediproto-sync/src/mastodon.rs
@@ -30,7 +30,7 @@ impl MastodonApiExtensions for Box<dyn megalodon::Megalodon + Send + Sync> {
             limit: limit_value,
             max_id: None,
             since_id: last_post_id,
-            pinned: Some(false),
+            pinned: Some(true),
             exclude_replies: Some(true),
             exclude_reblogs: Some(true),
             only_media: Some(false),
