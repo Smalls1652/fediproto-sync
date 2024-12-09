95d34a10661aa1e205bf3de318735a560418510e
Timothy Small <smalls.online@icloud.com>
2024-12-09 11:02:49 -0500

    Add code docs to type_impls

--- fediproto-sync/src/db/type_impls.rs
+++ fediproto-sync/src/db/type_impls.rs
@@ -1,17 +1,31 @@
 use diesel::{
-    deserialize::{self, FromSql, FromSqlRow}, query_builder::QueryId, serialize::{self, IsNull, ToSql}, sql_types::*, AsExpression
+    deserialize::{self, FromSql, FromSqlRow},
+    query_builder::QueryId,
+    serialize::{self, IsNull, ToSql},
+    sql_types::*,
+    AsExpression
 };
 
+/// Custom type for Diesel to handle UUIDs in both SQLite and PostgreSQL.
 #[derive(Copy, Clone, Debug, SqlType, QueryId)]
 #[diesel(postgres_type(name = "uuid"))]
 #[diesel(sqlite_type(name = "Text"))]
 pub struct MultiBackendUuid;
 
+/// Proxy type for `uuid::Uuid` to implement `FromSql` and `ToSql` for use with
+/// Diesel.
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, FromSqlRow, AsExpression)]
 #[diesel(sql_type = MultiBackendUuid)]
 pub struct UuidProxy(pub uuid::Uuid);
 
+// `UuidProxy` - SQLite `FromSql` and `ToSql` implementations.
+
 impl FromSql<MultiBackendUuid, diesel::sqlite::Sqlite> for UuidProxy {
+    /// Parse a `uuid::Uuid` from a SQLite `Text` column.
+    ///
+    /// ## Arguments
+    ///
+    /// * `bytes` - The raw value of the column.
     fn from_sql(
         bytes: <diesel::sqlite::Sqlite as diesel::backend::Backend>::RawValue<'_>
     ) -> deserialize::Result<Self> {
@@ -23,6 +37,11 @@ impl FromSql<MultiBackendUuid, diesel::sqlite::Sqlite> for UuidProxy {
 }
 
 impl ToSql<MultiBackendUuid, diesel::sqlite::Sqlite> for UuidProxy {
+    /// Serialize a `uuid::Uuid` to a SQLite `Text` column.
+    ///
+    /// ## Arguments
+    ///
+    /// * `out` - The output buffer to write the serialized value to.
     fn to_sql<'b>(
         &'b self,
         out: &mut serialize::Output<'b, '_, diesel::sqlite::Sqlite>
@@ -32,7 +51,19 @@ impl ToSql<MultiBackendUuid, diesel::sqlite::Sqlite> for UuidProxy {
     }
 }
 
+// `UuidProxy` - PostgreSQL `FromSql` and `ToSql` implementations.
+
 impl FromSql<MultiBackendUuid, diesel::pg::Pg> for UuidProxy {
+    /// Parse a `uuid::Uuid` from a PostgreSQL `uuid` column.
+    ///
+    /// ## Arguments
+    ///
+    /// * `value` - The raw value of the column.
+    ///
+    /// ## Note
+    ///
+    /// This implementation leverages the existing `FromSql` implementation for
+    /// `uuid::Uuid` for the PostgreSQL backend.
     fn from_sql(value: diesel::pg::PgValue<'_>) -> deserialize::Result<Self> {
         let uuid = <uuid::Uuid as FromSql<Uuid, diesel::pg::Pg>>::from_sql(value)?;
 
@@ -41,7 +72,20 @@ impl FromSql<MultiBackendUuid, diesel::pg::Pg> for UuidProxy {
 }
 
 impl ToSql<MultiBackendUuid, diesel::pg::Pg> for UuidProxy {
-    fn to_sql<'b>(&'b self, out: &mut diesel::serialize::Output<'b, '_, diesel::pg::Pg>) -> serialize::Result {
+    /// Serialize a `uuid::Uuid` to a PostgreSQL `uuid` column.
+    ///
+    /// ## Arguments
+    ///
+    /// * `out` - The output buffer to write the serialized value to.
+    ///
+    /// ## Note
+    ///
+    /// This implementation leverages the existing `ToSql` implementation for
+    /// `uuid::Uuid` for the PostgreSQL backend.
+    fn to_sql<'b>(
+        &'b self,
+        out: &mut diesel::serialize::Output<'b, '_, diesel::pg::Pg>
+    ) -> serialize::Result {
         <uuid::Uuid as ToSql<Uuid, diesel::pg::Pg>>::to_sql(&self.0, out)?;
 
         Ok(IsNull::No)
