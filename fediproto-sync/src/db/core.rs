7f5fe9cbfbf18eeab5bf0443ef1249466b43ddec
Timothy Small <smalls.online@icloud.com>
2024-12-09 11:08:01 -0500

    Add code docs to db::core

--- fediproto-sync/src/db/core.rs
+++ fediproto-sync/src/db/core.rs
@@ -3,6 +3,17 @@ pub const POSTGRES_MIGRATIONS: diesel_migrations::EmbeddedMigrations =
 pub const SQLITE_MIGRATIONS: diesel_migrations::EmbeddedMigrations =
     diesel_migrations::embed_migrations!("./migrations/sqlite");
 
+/// Run any pending database migrations.
+///
+/// ## Arguments
+///
+/// * `connection` - The database connection to run the migrations on.
+///
+/// ## Note
+///
+/// This is the main entry point for running database migrations. It will
+/// automatically determine the database backend and run the appropriate
+/// migrations.
 pub fn run_migrations(
     connection: &mut crate::db::AnyConnection
 ) -> Result<(), crate::error::Error> {
@@ -16,21 +27,29 @@ pub fn run_migrations(
     }
 }
 
-/// Run any pending database migrations for Postgres.
+/// Apply any migrations to the database.
 ///
 /// ## Arguments
 ///
 /// * `connection` - The database connection to run the migrations on.
+/// * `migrations` - The embedded migrations, specific to the database backend,
+///   to run.
+/// 
+/// ## Note
+/// 
+/// This function is a helper for `run_migrations` and should not be called
+/// directly.
 fn run_migrations_backend<T: diesel::backend::Backend + 'static>(
     connection: &mut impl diesel_migrations::MigrationHarness<T>,
     migrations: diesel_migrations::EmbeddedMigrations
 ) -> Result<(), crate::error::Error> {
-    let pending_migrations = connection.pending_migrations(migrations)
-        .map_err(|e| crate::error::Error::with_source(
+    let pending_migrations = connection.pending_migrations(migrations).map_err(|e| {
+        crate::error::Error::with_source(
             "Failed to get pending database migrations.",
             crate::error::ErrorKind::DatabaseMigrationError,
             e
-        ))?;
+        )
+    })?;
 
     if pending_migrations.is_empty() {
         tracing::info!("No pending database migrations.");
@@ -43,12 +62,13 @@ fn run_migrations_backend<T: diesel::backend::Backend + 'static>(
     );
 
     for migration_item in pending_migrations {
-        connection.run_migration(&migration_item)
-            .map_err(|e| crate::error::Error::with_source(
+        connection.run_migration(&migration_item).map_err(|e| {
+            crate::error::Error::with_source(
                 "Failed to run database migration.",
                 crate::error::ErrorKind::DatabaseMigrationError,
                 e
-            ))?;
+            )
+        })?;
 
         tracing::info!("Applied migration '{}'", migration_item.name());
     }
