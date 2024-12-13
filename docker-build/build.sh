d8d3b55a7b6dd1ad36b00bcf9c56afea0698326a
Timothy Small <smalls.online@icloud.com>
2024-12-13 10:59:24 -0500

    Revert back to OpenSSL and fix cross-compilation (#16)
    * Revert back to how container builds were done
    
    * Revert boring back to openssl

--- docker-build/build.sh
+++ docker-build/build.sh
@@ -8,9 +8,12 @@ case "${TARGETPLATFORM}" in
         export CC_x86_64_unknown_linux_gnu="x86_64-linux-gnu-gcc"
         export CXX_x86_64_unknown_linux_gnu="x86_64-linux-gnu-g++"
 
+        #export X86_64_UNKNOWN_LINUX_GNU_OPENSSL_LIB_DIR="/usr/lib/x86_64-linux-gnu/"
+        #export X86_64_UNKNOWN_LINUX_GNU_OPENSSL_INCLUDE_DIR=""
+
         dpkg --add-architecture amd64
         apt-get update
-        apt-get install -y libpq-dev:amd64 libpq5:amd64 libsqlite3-dev:amd64 libsqlite3-0:amd64
+        apt-get install -y libpq-dev:amd64 libpq5:amd64 libsqlite3-dev:amd64 libsqlite3-0:amd64 libssl-dev:amd64
         apt-get clean
         rm -rf /var/lib/apt/lists/*
         ;;
@@ -22,7 +25,7 @@ case "${TARGETPLATFORM}" in
 
         dpkg --add-architecture arm64
         apt-get update
-        apt-get install -y libpq-dev:arm64 libpq5:arm64 libsqlite3-dev:arm64 libsqlite3-0:arm64
+        apt-get install -y libpq-dev:arm64 libpq5:arm64 libsqlite3-dev:arm64 libsqlite3-0:arm64 libssl-dev:arm64
         apt-get clean
         rm -rf /var/lib/apt/lists/*
         ;;
@@ -36,6 +39,7 @@ echo "${RUST_TARGET}"
 
 export FEDIPROTOSYNC_INCLUDE_COMMIT_HASH="true"
 export FEDIPROTOSYNC_UPDATE_MANIFEST_VERSION="true"
+export PKG_CONFIG_SYSROOT_DIR=/
 
 rustup default nightly
 rustup target add --toolchain "nightly" "${RUST_TARGET}"
--- fediproto-sync/src/crypto.rs
+++ fediproto-sync/src/crypto.rs
@@ -5,19 +5,19 @@
 /// 
 /// * `public_key` - The public key to use for encryption.
 pub fn encrypt_string(
-    public_key: &boring::rsa::Rsa<boring::pkey::Public>,
+    public_key: &openssl::rsa::Rsa<openssl::pkey::Public>,
     input_string: &str
 ) -> Result<String, crate::error::Error> {
     let mut encrypted_string = vec![0; public_key.size() as usize];
 
-    public_key.public_encrypt(input_string.as_bytes(), &mut encrypted_string, boring::rsa::Padding::PKCS1)
+    public_key.public_encrypt(input_string.as_bytes(), &mut encrypted_string, openssl::rsa::Padding::PKCS1)
         .map_err(|e| crate::error::Error::with_source(
             "Failed to encrypt string",
             crate::error::ErrorKind::EncryptionError,
             Box::new(e)
         ))?;
 
-    let encoded_string = boring::base64::encode_block(&encrypted_string);
+    let encoded_string = openssl::base64::encode_block(&encrypted_string);
 
     Ok(encoded_string)
 }
@@ -28,19 +28,19 @@ pub fn encrypt_string(
 /// 
 /// * `private_key` - The private key to use for decryption.
 pub fn decrypt_string(
-    private_key: &boring::rsa::Rsa<boring::pkey::Private>,
+    private_key: &openssl::rsa::Rsa<openssl::pkey::Private>,
     input_string: &str
 ) -> Result<String, crate::error::Error> {
     let mut decrypted_string = vec![0; private_key.size() as usize];
 
-    let encrypted_string = boring::base64::decode_block(input_string)
+    let encrypted_string = openssl::base64::decode_block(input_string)
         .map_err(|e| crate::error::Error::with_source(
             "Failed to decode base64 string",
             crate::error::ErrorKind::DecryptionError,
             Box::new(e)
         ))?;
 
-    let decrypted_length = private_key.private_decrypt(&encrypted_string, &mut decrypted_string, boring::rsa::Padding::PKCS1)
+    let decrypted_length = private_key.private_decrypt(&encrypted_string, &mut decrypted_string, openssl::rsa::Padding::PKCS1)
         .map_err(|e| crate::error::Error::with_source(
             "Failed to decrypt string",
             crate::error::ErrorKind::DecryptionError,
