5ee47ba20681cb2cb191d2f057a13561bbe1710d
Timothy Small <smalls.online@icloud.com>
2024-12-23 17:13:13 -0500

    Remove atprotolib-rs
    🫡

--- atprotolib-rs/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! This crate provides a library of types and functions to interact with the
-//! ATProtocol.
-
-/// Contains types related to the ATProtocol.
-pub mod types;
-
-#[cfg(feature = "apicalls")]
-/// Contains structs and functions to help with making API calls
-pub mod api_calls;
--- atprotolib-rs/src/types/com_atproto/moderation/mod.rs
+++
@@ -1,5 +0,0 @@
-#[cfg(feature = "apicalls")]
-pub mod api_calls;
-
-pub mod api_requests;
-pub mod api_responses;
