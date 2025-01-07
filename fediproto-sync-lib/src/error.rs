e45ceea533d5c184cc9ce963e5390dc48c684887
Timothy Small <smalls.online@icloud.com>
2025-01-06 23:45:51 -0500

    Update to use thiserror

--- fediproto-sync-lib/src/error.rs
+++ fediproto-sync-lib/src/error.rs
@@ -1,99 +1,68 @@
 use thiserror::Error;
 
 /// Error value for FediProtoSync.
-#[derive(Error, Debug)]
+#[derive(Error, Debug, Clone)]
 #[allow(dead_code)]
-pub struct FediProtoSyncError {
-    /// A message describing the error.
-    pub message: String,
-
-    /// The kind of error that occurred.
-    pub kind: FediProtoSyncErrorKind
-}
-
-#[allow(dead_code)]
-impl FediProtoSyncError {
-    /// Create a new error.
-    ///
-    /// ## Arguments
-    ///
-    /// * `message` - A message describing the error.
-    /// * `kind` - The kind of error that occurred.
-    pub fn new(
-        message: &str,
-        kind: FediProtoSyncErrorKind
-    ) -> Self {
-        Self {
-            message: message.to_string(),
-            kind
-        }
-    }
-}
-
-impl std::fmt::Display for FediProtoSyncError {
-    fn fmt(
-        &self,
-        f: &mut std::fmt::Formatter
-    ) -> std::fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-/// The kind of error that occurred.
-#[derive(Debug, Clone)]
-#[allow(dead_code)]
-pub enum FediProtoSyncErrorKind {
+pub enum FediProtoSyncError {
     /// An error occurred while trying to read an environment variable.
-    EnvironmentVariableError,
+    #[error("Failed to read environment variable: {0}")]
+    EnvironmentVariableError(String),
+
+    /// An error occurred while trying to parse an environment variable.
+    #[error("Failed to parse environment variable: {0}")]
+    EnvironmentVariableParseError(String),
 
     /// An error occurred while trying to authenticate.
-    AuthenticationError,
-
-    /// An error occurred while trying to connect to the database.
-    DatabaseConnectionError,
-
-    /// An error occurred while trying to run a database migration.
-    DatabaseMigrationError,
+    #[error("Failed to authenticate to {0}.")]
+    AuthenticationError(AuthenticationSource),
 
     /// An invalid database type was specified.
+    #[error("Invalid database type.")]
     InvalidDatabaseType,
 
-    /// An error occurred while querying the database.
-    DatabaseQueryError,
-
-    /// An error occurred while trying to insert a new record into the database.
-    DatabaseInsertError,
-
-    /// An error occurred while trying to delete a database record.
-    DatabaseDeleteError,
-
     /// An error occurred while encrypting a value.
+    #[error("Failed to encrypt value.")]
     EncryptionError,
 
     /// An error occurred while decrypting a value.
+    #[error("Failed to decrypt value.")]
     DecryptionError,
 
     /// An error occurred while creating a HTTP client.
+    #[error("Failed to create HTTP client.")]
     HttpClientCreationError,
 
     /// An error occurred while uploading a video to BlueSky.
+    #[error("Failed to upload video.")]
     VideoUploadError,
 
     /// An error occurred while removing a temporary file.
+    #[error("Failed to remove temporary file.")]
     TempFileRemovalError,
 
     /// An error occurred while running the web server.
+    #[error("An error occurred with the web server.")]
     WebServerError,
 
     /// An error occurred while trying to compress an image.
+    #[error("Failed to compress image.")]
     ImageCompressionError
 }
 
-impl std::fmt::Display for FediProtoSyncErrorKind {
+#[derive(Debug, Clone)]
+pub enum AuthenticationSource {
+    Mastodon,
+    BlueSky
+}
+
+impl std::fmt::Display for AuthenticationSource {
     fn fmt(
         &self,
-        f: &mut std::fmt::Formatter
+        f: &mut std::fmt::Formatter<'_>
     ) -> std::fmt::Result {
-        write!(f, "{:?}", self)
+        match self {
+            AuthenticationSource::Mastodon => write!(f, "Mastodon"),
+            AuthenticationSource::BlueSky => write!(f, "BlueSky")
+        }
     }
 }
