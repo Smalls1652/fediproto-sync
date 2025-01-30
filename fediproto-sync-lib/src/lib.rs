3e4b0e60ce06feae37e74ce81d37ef6de080d55f
Timothy Small <smalls.online@icloud.com>
2025-01-30 13:10:17 -0500

    Add initial tests for fediproto-sync-lib (#68)
    * Add rstest to fediproto-sync-lib
    
    * Add initial tests for fediproto-sync-lib
    
    * Update dev tooling configs
    
    * Add test workflow

--- fediproto-sync-lib/src/lib.rs
+++ fediproto-sync-lib/src/lib.rs
@@ -7,8 +7,14 @@ pub mod crypto;
 /// Error types for Fediproto Sync.
 pub mod error;
 
+/// Tests for the `fediproto-sync-lib` crate.
+#[cfg(test)]
+#[allow(non_snake_case)]
+mod tests;
+
 /// Generic utilities for FediProto Sync.
 pub mod utils;
+
 
 /// Version information generated from git metadata.
 pub const GIT_VERSION: &str = std::env!("GIT_VERSION");
